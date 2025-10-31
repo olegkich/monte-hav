0428a154a3bc56bfade53bad83d92997dec787b1
olegkich8 <olegkich8@gmail..com>
2025-10-31 19:07:46 +0100

    add helper functions, implement select(), start building expand() for MCTS

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,5 +1,6 @@
 
 use crate::{board::{self, BoardState}, win_detector};
+use rand::Rng;
 
 struct Node {
     state: BoardState,
@@ -51,17 +52,52 @@ impl MCTS {
         }
     }
 
-    fn search(&self, start_state: BoardState) {
-        let root_node = Node::new(start_state, None);
+    fn search(&mut self, start_state: BoardState) {
+      
+        let root_index = self.nodes.len();
+        self.nodes.push(Node::new(start_state, None));
 
+        for _ in 0..self.max_iter {
+            let node_index = self.select(root_index);
+            self.expand(node_index);
+        }
         
     }
 
-    fn select() {
+    fn select(&self, start_index: usize) -> usize {
+        let node = &self.nodes[start_index];
 
+        if node.children.is_empty() {
+            return start_index;
+        }
+
+        // WARNING: (MILD) index 0 is unsafe but with NEG_INFINITY UCT the first child picked should always overwrite it
+        let (mut best_uct,mut best_index): (f32, usize) = (f32::NEG_INFINITY, 0);
+
+        for index in &node.children {
+            if let Some(child_node) = self.nodes.get(*index) {
+                let uct = self.calculate_uct(child_node, node.visits);
+                
+                if (uct > best_uct) {
+                    best_uct = uct;
+                    best_index = *index;
+                }
+            };   
+        };
+
+        return self.select(best_index);
     }
 
-    fn calculate_uct(&self, node: &Node, parent_visits: f32) -> f32 {
+    fn expand(&mut self, node_index: usize) {
+
+        if let Some(node) = self.nodes.get_mut(node_index) {
+            let moves = node.state.legal_moves();
+
+
+        };
+    }
+
+    fn calculate_uct(&self, node: &Node, parent_visits: u32) -> f32 {
         if node.visits == 0 {
             // to encourage exploring unvisited nodes
             return f32::INFINITY;
@@ -71,13 +107,7 @@ impl MCTS {
         let n_i = node.visits as f32;
         let c = self.exploration_constant;
 
-        (w_i / n_i) + c * ((parent_visits.ln() / n_i).sqrt())
+        (w_i / n_i) + c * (((parent_visits as f32).ln() / n_i).sqrt())
     }
 
-    fn get_node_by_index(&self, i: Option<usize>) -> Option<&Node> {
-        match i {
-            None => None,
-            Some(index) => self.nodes.get(index),
-        }
-    }
 }
